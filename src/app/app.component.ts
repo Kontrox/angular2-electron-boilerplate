e25c3d926be5f586a5bed8f1ce81031ff2fc3ebb
Erlend <erlentob@stud.ntnu.no>
2016-11-01 14:48:08 +0100

    Remove boilerplate components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,18 +8,4 @@ const ipcRenderer = require('electron').ipcRenderer;
   selector: 'my-app',
   templateUrl: 'app.component.html'
 })
-export class AppComponent {
-  constructor() {
-    ipcRenderer.on("reply", (event, arg) => {
-      console.log("Reply was " + arg);
-    });
-  }
-
-  public text_hello_world: string = "Hello Angular 2!";
-  public text_small: string = "Greatness awaits..."
-
-  public test(): void {
-    console.log("Getestet");
-    ipcRenderer.send("message", "tested");
-  }
-}
+export class AppComponent { }
