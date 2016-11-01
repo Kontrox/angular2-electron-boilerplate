a7c3c08987195a0b79b5bf3a77663490eedd6f8e
Erlend <erlentob@stud.ntnu.no>
2016-11-01 14:48:30 +0100

    Seperate html and css

--- src/app/powers/card.component.ts
+++ src/app/powers/card.component.ts
@@ -3,7 +3,8 @@ import { Input, Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'card',
-  templateUrl: 'card.component.html'
+  templateUrl: 'card.component.html',
+  styleUrls: ['card.component.css']
 })
 export class CardComponent {
   @Input()
